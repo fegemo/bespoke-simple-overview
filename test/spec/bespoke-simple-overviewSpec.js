1be4baeec784a5fa893c5745f2b0d58920c16f4c
Flavio Coutinho <fegemo@gmail.com>
2016-02-22 17:03:05 -0300

    Tests covering 100%

--- test/spec/bespoke-simple-overviewSpec.js
+++ test/spec/bespoke-simple-overviewSpec.js
@@ -1,16 +1,18 @@
-Function.prototype.bind = Function.prototype.bind || require('function-bind');
-
 var bespoke = require('bespoke'),
-  overview = require('../../lib-instrumented/bespoke-simple-overview.js'),
+  simpleOverview = require('../../lib-instrumented/bespoke-simple-overview.js'),
+  bullets = require('bespoke-bullets'),
   simulant = require('simulant'),
 
   deck,
-  createDeck = function(options) {
+  createDeck = function(options, classOnParent) {
     var parent = document.createElement('article');
     parent.innerHTML = '<section></section><section></section>';
+    if (typeof classOnParent === 'string') {
+      parent.classList.add(classOnParent)
+    }
 
     deck = bespoke.from(parent, [
-      overview(options)
+      simpleOverview(options)
     ]);
   },
   pressKey = function(which) {
@@ -32,10 +34,20 @@ describe('bespoke-simple-overview', function() {
     });
   });
 
+  describe('when NOT STYLED by the plugin', function() {
+    it('should NOT inject CSS onto the document', function() {
+      var numberOfStylesheets = document.styleSheets.length;
+      createDeck({ insertStyles: false });
+      expect(document.styleSheets.length).toEqual(numberOfStylesheets);
+    });
+  });
+
   describe('options', function() {
-    beforeEach(createDeck.bind(null, {
-      activationKey: 'd'
-    }));
+    beforeEach(function() {
+      createDeck({
+        activationKey: 'd'
+      });
+    });
 
     it('should allow configuration for the activation key ("ESC")', function() {
       pressKey('d');
@@ -90,6 +102,44 @@ describe('bespoke-simple-overview', function() {
         deck.fire('simple-overview.toggle');
         expect(deck.parent.classList.contains('bespoke-simple-overview')).toBe(false);
       });
+    });
+
+    describe('"destroy"', function() {
+      it('should detach the onkeydown handler of the window object when the deck receives the "destroy" event', function() {
+        deck.fire('destroy');
+        expect(window.onkeydown).toBe(null);
+      });
+    });
+  });
+
+  describe('navigating with bespoke-bullets', function() {
+    beforeEach(function() {
+      var parent = document.createElement('article');
+      parent.innerHTML = '<section><ul><li>one</li><li>two</li></ul></section><section></section>';
+
+      deck = bespoke.from(parent, [
+        simpleOverview(),
+        bullets('li')
+      ]);
+    });
+
+    it('should IGNORE the bullets inside a slide when in overview mode', function() {
+      pressKey(27);
+      deck.next();
+      expect(deck.slide()).toBe(1);
+    });
+
+    it('should NOT IGNORE the bullets inside a slide when in regular mode', function() {
+      deck.next();
+      expect(deck.slide()).toBe(0);
+      deck.next()
+      expect(deck.slide()).toBe(1);
+    });
+
+    it('should NOT NAVIGATE to invalid slide indices in overview mode', function() {
+      pressKey(27);
+      deck.prev();
+      expect(deck.slide()).toBe(0);
     });
   });
 });
